c463c0a3430d98aa819b6614fec73c2b02e990a1
Prince Narteh <prinarteh@gmail.com>
2022-04-30 14:38:33 +0000

    feat: adds apollo-graphql to the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,30 @@
 import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import express from "express";
+import {ApolloServer, gql} from "apollo-server-express";
 import cors from "cors";
 import {expressJwt} from "./utils/expressJwt";
 import {logger} from "./utils/logger";
+import resolvers from "./graphql/resolvers";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors(), express.json(), expressJwt);
 
-app.listen(PORT, () => {
-  logger.info(`Server running on http://localhost:${PORT}`);
-});
+const typeDefs = gql(
+  fs.readFileSync(path.join(__dirname, "graphql", "schema.graphql"), "utf-8")
+);
+
+const start = async () => {
+  const apolloServer = new ApolloServer({typeDefs, resolvers});
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app, path: "/graphql"});
+
+  app.listen(PORT, () => {
+    logger.info(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+start();
