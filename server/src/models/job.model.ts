89505ed6adf142ed9f982ac6022637206ff32f35
Prince Narteh <prinarteh@gmail.com>
2022-04-30 14:51:19 +0000

    adds Job model

---
+++ server/src/models/job.model.ts
@@ -0,0 +1,22 @@
+import {Schema, Model, Document, model} from "mongoose";
+
+interface IJob {
+  title: string;
+  description: string;
+}
+
+interface IJobDocument extends IJob, Document {}
+interface IJobModel extends Model<IJobDocument> {}
+
+const jobSchema: Schema<IJobDocument> = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Job = model<IJobDocument, IJobModel>("Job", jobSchema);
