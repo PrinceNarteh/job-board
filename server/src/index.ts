da11c487594d5c18b416c5d937162a1261e40819
Prince Narteh <prinarteh@gmail.com>
2022-04-30 13:25:12 +0000

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
