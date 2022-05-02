f6fc7092e08355d96d426edc8ebb28e3895138ec
Prince Narteh <prinarteh@gmail.com>
2022-05-02 13:54:37 +0000

    feat: adds routing

---
+++ client/src/Router.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const jobs = ["Frontend Developer", "Backend Developer", "Fullstack Developer"];
+
+const Home = () => {
+  return (
+    <div className="h-[calc(100vh_-_56px)] max-w-5xl mx-auto px-10 2xl:px-0 rounded-md">
+      <h1 className="text-5xl py-10 font-bold italic text-gray-500">
+        Job Board
+      </h1>
+
+      <ul className="w-full shadow-md px-5 py-3 border border-gray-200">
+        {jobs.map((job, idx) => (
+          <li
+            key={idx}
+            className="text-xl font-medium text-gray-700 border-b border-gray-300 py-5 pl-2 hover:bg-slate-100 last:border-b-0 cursor-pointer">
+            {job}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
